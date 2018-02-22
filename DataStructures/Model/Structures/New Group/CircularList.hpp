746b70b65d9df742723bb70b6676e96bbb9a5719
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-02-22 08:56:58 -0700

    Created the Stack method.

---
+++ DataStructures/Model/Structures/New Group/CircularList.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularList.hpp
+//  DataStructures
+//
+//  Created by Harris, Jacob on 2/22/18.
+//  Copyright © 2018 Harris, Jacob. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include <stdio.h>
+
+#endif /* CircularList_hpp */
---
+++ DataStructures/Model/Structures/New Group/Queue.hpp
@@ -0,0 +1,14 @@
+//
+//  Queue.hpp
+//  DataStructures
+//
+//  Created by Harris, Jacob on 2/22/18.
+//  Copyright © 2018 Harris, Jacob. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <stdio.h>
+
+#endif /* Queue_hpp */
