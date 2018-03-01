56a08435e9c563bc6a64a2592d255238fbf6d158
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-03-01 07:48:38 -0700

    added stacksandqueues

---
+++ Testing/TestingStacksAndQueues.hpp
@@ -0,0 +1,27 @@
+//
+//  TestingStacksAndQueues.hpp
+//  DataStructures
+//
+//  Created by Harris, Jacob on 2/28/18.
+//  Copyright © 2018 Harris, Jacob. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include "Timer.hpp"
+#include "FileController.hpp"
+#include "Queue.hpp"
+#include "Stack.hpp"
+#include "List.hpp"
+#include <iostream>
+using namespace std;
+
+class TestingStacksAndQueues
+{
+public:
+    stackVersusList();
+    stackVersusQueue();
+    queueVersusList();
+};
+#endif /* TestingStacksAndQueues_hpp */
