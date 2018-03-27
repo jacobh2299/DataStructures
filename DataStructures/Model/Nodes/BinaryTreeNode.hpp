55a79e3e8e9c119b293c59c0a2a0c0b5d44d9927
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-03-27 09:45:49 -0600

    FIxed my treeNode.hpp! GO ME

--- DataStructures/Model/Nodes/BinaryTreeNode.hpp
+++ DataStructures/Model/Nodes/BinaryTreeNode.hpp
@@ -9,13 +9,72 @@
 #ifndef BinaryTreeNode_hpp
 #define BinaryTreeNode_hpp
 
-#include "Node.hpp"
+#include "/Users/jhar3975/Documents/Swift/DataStructures/DataStructures/Model/Nodes/Node.hpp"
 
 template <class Type>
 class BinaryTreeNode : public Node<Type>
 {
 private:
     BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * left;
+    BinaryTreeNode<Type> * right;
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setRightNode(BinaryTreeNode<Type> * right);
+    void setLeftNode(BinaryTreeNode<Type> * left);
+
+    BinaryTreeNode<Type> * getRootNode();
+    BinaryTreeNode<Type> * getLeftNode();
+    BinaryTreeNode<Type> * getRightNode();
+
 };
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
+{
+    return this->root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
+{
+    return this->right;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftNode()
+{
+    return this->left;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRootNode(BinaryTreeNode<Type> * newRoot)
+{
+    root = newRoot;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightNode(BinaryTreeNode<Type> * right)
+{
+    this->right = right;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftNode(BinaryTreeNode<Type> * left)
+{
+    this->left = left;
+}
+
 
 #endif /* BinaryTreeNode_hpp */
